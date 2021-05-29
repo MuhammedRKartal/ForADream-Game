ca3917f59409f619d429e3ce9f3973b525d4377c
ermeteatakan <ermeteat@gmail.com>
2021-05-29 16:16:58 +0300

    oyunlar, siyah gölgeler ve bar değişti

---
+++ Oyunlar/Ping Pong Game/Assets/dusman.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dusman : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        Launch();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void Launch()
+    {
+        
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(0, speed * y);
+    }
+}
