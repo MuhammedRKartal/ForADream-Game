42eaf7012582d75b244362d370d8055b4e877ad4
ayyucecay <61210641+ayyucecay@users.noreply.github.com>
2021-05-30 23:02:16 +0300

    isolation

---
+++ Isolation Game/Assets/GridSizeChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class GridSizeChanger : MonoBehaviour
+{
+
+    private void Start()
+    {
+        StartCoroutine(MakeGrid());
+    }
+
+    private IEnumerator MakeGrid()
+    {
+        yield return new WaitForSeconds(0.25f);
+        GameManager.Instance.ChangeGridSize(3, 3);
+    }
+}
