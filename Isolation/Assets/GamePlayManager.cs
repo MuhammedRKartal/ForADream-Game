8af5e78315f75360db6008be41d14006ec63de9a
ayyucecay <61210641+ayyucecay@users.noreply.github.com>
2021-05-30 17:26:17 +0300

    ısolation

---
+++ Isolation/Assets/GamePlayManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public enum Gameplay
+{
+    AI = 0,
+    Random = 1,
+    Player = 2
+}
---
+++ Isolation/Assets/InfoMessage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InfoMessage : MonoBehaviour
+{
+    private TMP_Text textComp;
+
+    private void Awake()
+    {
+        textComp = GetComponent<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.onGameStarted += PrintMessagePlayer1;
+        GameManager.Instance.onTurnChanged += onTurnChanged;
+        GameManager.Instance.onEndState += onEndState;
+    }
+
+    private void onEndState(EndState endState)
+    {
+        if(endState == EndState.Player1Won)
+            textComp.text = "WIN!";
+        else if(endState == EndState.Player2Won)
+            textComp.text = "LOST";
+        else if (endState == EndState.Draw)
+            textComp.text = "Draw!";
+
+    }
+
+      private void onTurnChanged(PlayerPawn player)
+       {
+           if(player.playerNum == PlayerNum.Player1)
+               textComp.text = "";
+           else
+               textComp.text = "";
+       }
+
+    private void PrintMessagePlayer1()
+    {
+        textComp.text = "";
+    }
+}
