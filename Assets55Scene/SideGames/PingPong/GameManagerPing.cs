6dda2f69ea43d7b14a231c04365c0189f25ccceb
Muhammed Kartal <48036846+MuhammedRKartal@users.noreply.github.com>
2021-06-01 00:05:54 +0300

    55

---
+++ Assets55Scene/SideGames/PingPong/GameManagerPing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerPing : MonoBehaviour {
+
+	public static int PlayerScore1 = 0;
+	public static int PlayerScore2 = 0;
+	public string sname;
+	public GUISkin layout;
+
+	GameObject theBall;
+
+	// Use this for initialization
+	void Start () {
+		theBall = GameObject.FindGameObjectWithTag ("Ball");
+	}
+
+	public static void Score(string wallID) {
+		if (wallID == "RightWall") {
+			PlayerScore1++;
+		} else {
+			PlayerScore2++;
+		}
+	}
+
+	void OnGUI() {
+		GUI.skin = layout;
+		GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+		GUI.Label (new Rect (Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+
+		if (PlayerScore1 == 3) {
+			SceneManager.LoadScene(sceneName: sname);
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		} 
+		else if (PlayerScore2 == 3) {
+			PlayerScore1 = 0;
+			PlayerScore2 = 0;
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		}
+	}
+
+}
---
+++ Assets55Scene/SideGames/TowerBox/Scripts/GameplayController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayController : MonoBehaviour
+{
+    public static GameplayController instance;
+    public BoxSpawner box_Spawner;
+    [HideInInspector]
+    public BoxScript currentBox;
+
+    public CameraFollow cameraScript;
+    private int moveCount;
+
+
+    public Text countDown;
+
+
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        box_Spawner.SpawnBox();
+        
+    }
+
+    
+    void Update()
+    {
+        DetectInput();
+        
+    }
+    void DetectInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentBox.DropBox();
+        }
+    }
+
+    public void SpawnNewBox()
+    {
+        Invoke("NewBox", 0.000000001f);
+    }
+
+    void NewBox()
+    {
+        box_Spawner.SpawnBox();
+    }
+
+    public void MoveCamera()
+    {
+        moveCount++;
+        if (moveCount == 3)
+        {
+            cameraScript.targetPos.y += 2f;          
+        }
+        if (moveCount == 5)
+        {
+            
+                Time.timeScale = 0f;
+                countDown.text = "WIN!!";
+            
+            
+        }
+    }
+  
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    
+}
